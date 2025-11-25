4633b98a517e7036b2a90681be08a324d09c8a08
microtasker-c <twmina99@163.com>
2025-11-25 22:18:47 +0800

    init style

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
-<script setup lang="ts"></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="container">
+    <h1 class="font-weight-100 text-center">问卷系统</h1>
+    <!-- 按钮组 -->
+    <div class="mb-15">
+      <el-button type="primary" :icon="Plus">创建问卷</el-button>
+      <el-button type="success" :icon="Compass">组件市场</el-button>
+    </div>
+    <!-- 数据表格 -->
+
+    <el-table :data="tableData" style="width: 100%" border></el-table>
+  </div>
 </template>
+
+<script setup lang="ts">
+import { Plus, Compass } from '@element-plus/icons-vue'
+import { ref } from 'vue'
+const tableData = ref()
+</script>
 
 <style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,19 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+// 引入element plus
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+// 中文化的支持
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+
+// 样式
+import './assets/css/index.scss'
+
 const app = createApp(App)
 
-app.use(createPinia())
-app.use(router)
+app.use(createPinia()).use(router).use(ElementPlus, {
+  locale: zhCn,
+})
 
 app.mount('#app')
